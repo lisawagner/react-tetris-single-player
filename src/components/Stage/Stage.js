23700c79e59d6e49b37732ed52003b3d0dc4c273
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-14 20:02:30 -0700

    scaffold out basic project

---
+++ src/components/Stage/Stage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// Components
+import Cell from "../Cell/Cell";
+
+const Stage = ({ stage }) => (
+  <div>
+    <Cell />
+  </div>
+);
+
+export default Stage;
---
+++ src/components/Tetris/Tetris.js
@@ -0,0 +1,22 @@
+import React from "react";
+// Components
+import Stage from "../Stage/Stage";
+import Display from "../Display/Display";
+import StartButton from "../StartButton/StartButton";
+
+const Tetris = () => {
+  return (
+    <div>
+      <Stage />
+      <aside>
+        <div>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
+        </div>
+      </aside>
+    </div>
+  );
+};
+
+export default Tetris;
