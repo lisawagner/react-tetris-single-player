79f29abc94b006b6464afb0395b862fd3a6bec31
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-14 19:36:42 -0700

    added interval hook

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function interval() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(interval, delay);
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [delay]);
+}
